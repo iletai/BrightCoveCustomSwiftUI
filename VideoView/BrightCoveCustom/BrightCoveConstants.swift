075f95bdd4dfc6fa6e7206b9782ecf517af6218f
Lê Quang Trọng Tài <iletai@hotmail.com>
2022-06-10 13:22:32 +0700

    + add swiftlint rule

---
+++ VideoView/BrightCoveCustom/BrightCoveConstants.swift
@@ -0,0 +1,14 @@
+//
+//  BrightCoveConstants.swift
+//  VideoView
+//
+//  Created by Lê Quang Trọng Tài on 6/10/22.
+//
+
+import Foundation
+
+struct BrightCoveConstants {
+    static let AccountID = "5434391461001"
+    static let PolicyKey = "BCpkADawqM0T8lW3nMChuAbrcunBBHmh4YkNl5e6ZrKQwPiK_Y83RAOF4DP5tyBF_ONBVgrEjqW6fbV0nKRuHvjRU3E8jdT9WMTOXfJODoPML6NUDCYTwTHxtNlr5YdyGYaCPLhMUZ3Xu61L"
+    static let VideoId = "5702141808001"
+}
